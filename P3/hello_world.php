ac05f824e6dc13fdc1a876fabfd893ad90a82c77
JAntonioVR <juanantoniovr98@gmail.com>
2022-04-12 13:34:46 +0200

    Comienzo de la prácica de PHP

---
+++ P3/hello_world.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Prueba de PHP</title>
+    </head>
+    <body>
+        <p>Esto es una línea de HTML.</p>
+        <?php
+        echo "<p>Y esto una línea de PHP.</p>";
+        # phpinfo();
+        echo '<p>Nueva \n linea</p>';
+        ?>
+    </body>
+</html>
